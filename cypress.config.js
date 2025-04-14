174f519675f94358eba69afda747fed61bce21fc
Rashed <rashedrion81@gmail.com>
2025-04-14 01:15:40 -0700

    scriptsUpdate

--- cypress.config.js
+++ cypress.config.js
@@ -15,8 +15,4 @@ module.exports = defineConfig({
   env: {
     URL: "https://naveenautomationlabs.com/opencart/index.php?route=account/register",
   },
-
-  {
-    "include": ["./node_modules/cypress", "cypress/**/*.js"]
-  }
 });
